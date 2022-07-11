71d7721dec0d9a8c0fca9cc55ffcbe6333e61b09
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-07-11 08:07:21 +0100

    jsway
    update jsway array methods

--- js/main.js
+++ js/main.js
@@ -283,46 +283,46 @@
 
 // TODO: create the character object here
 
-const aurora = {
-  name: "Aurora",
-  health: 150,
-  strength: 25,
-  xp: 0,
+// const aurora = {
+//   name: "Aurora",
+//   health: 150,
+//   strength: 25,
+//   xp: 0,
 
-  // Return the character description
-  describe() {
-    return `${this.name} has ${this.health} health points and ${
-      this.strength
-    } as strength. She has ${this.xp} experience points and needs ${
-      1000 - this.xp
-    } to next level.`;
-  },
-};
+//   // Return the character description
+//   describe() {
+//     return `${this.name} has ${this.health} health points and ${
+//       this.strength
+//     } as strength. She has ${this.xp} experience points and needs ${
+//       1000 - this.xp
+//     } to next level.`;
+//   },
+// };
 
-// Aurora is harmed by an arrow
-aurora.health -= 20;
+// // Aurora is harmed by an arrow
+// aurora.health -= 20;
 
-// Aurora equips a strength necklace
-aurora.strength += 10;
+// // Aurora equips a strength necklace
+// aurora.strength += 10;
 
-// Aurora learn a new skill
-aurora.xp += 15;
+// // Aurora learn a new skill
+// aurora.xp += 15;
 
-console.log(aurora.describe());
+// console.log(aurora.describe());
 
-// TODO: create the dog object here
+// // TODO: create the dog object here
 
-const dog = {
-  name: "Hokku",
-  species: "Shibe",
-  size: "1' 3\"",
-  bark() {
-    return "Woof! OwO";
-  },
-};
+// const dog = {
+//   name: "Hokku",
+//   species: "Shibe",
+//   size: "1' 3\"",
+//   bark() {
+//     return "Woof! OwO";
+//   },
+// };
 
-console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
-console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
+// console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+// console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
 
 // const circleRadius = function () {
 //   return Number(prompt("Enter the circle radius:"));
@@ -505,16 +505,16 @@ console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
 // A common data structure is the list (not to be confused with array).
 // A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
 
-let list = {
-  value: 1,
-  rest: {
-    value: 2,
-    rest: {
-      value: 3,
-      rest: null,
-    },
-  },
-};
+// let list = {
+//   value: 1,
+//   rest: {
+//     value: 2,
+//     rest: {
+//       value: 3,
+//       rest: null,
+//     },
+//   },
+// };
 // The resulting objects form a chain, like this:
 
 // A linked list
@@ -605,4 +605,350 @@ let list = {
 // console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
 // // // → true
 
+// **************************************************************************************************
+// Javascript.info
+//
+// array-methods
+//
+//////////////////
 
+// Tasks
+// Translate border-left-width to borderLeftWidth
+
+// let borderValue = "borderLeft-Width";
+
+// borderValue = borderValue.replace("-", "");
+
+// // importance: 5
+// // Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased
+// // “myShortString”.
+
+// function camelize(str) {
+//   let outputString;
+//   let processArray = str.split("-");
+
+//   for (let i = 1; i < processArray.length; i++) {
+//     processArray[i] =
+//       processArray[i].charAt(0).toUpperCase() + processArray[i].substring(1);
+//     console.log(processArray[i]);
+//   }
+//   outputString = processArray.join("");
+//   return outputString;
+// }
+// console.log(camelize("my-short-string"));
+
+// // That is: removes all dashes, each word after dash becomes uppercased.
+
+// // Examples:
+// // console.log(camelize("background-color"));
+// // == 'backgroundColor';
+// // camelize("list-style-image") == 'listStyleImage';
+// // camelize("-webkit-transition") == 'WebkitTransition';
+// // P.S. Hint: use split to split the string into an array, transform it and join back.
+
+// // Open a sandbox with tests.
+
+// // solution
+// // Filter range
+// // importance: 4
+
+// // Write a function filterRange(arr, a, b) that gets an array arr,
+// // looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+
+// function filterRange(arr, a, b) {
+//   return arr.filter((element) => element >= a && element <= b);
+// }
+
+// console.log(filterRange([5, 3, 8, 1], 1, 4));
+
+// // The function should not modify the array. It should return the new array.
+
+// // For instance:
+
+// // let arr = [5, 3, 8, 1];
+
+// // let filtered = filterRange(arr, 1, 4);
+
+// // alert( filtered ); // 3,1 (matching values)
+
+// // alert( arr ); // 5,3,8,1 (not modified)
+// // Open a sandbox with tests.
+
+// // solution
+// // Filter range "in place"
+// // importance: 4
+// // Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except
+// // those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+
+// function filterRangeInPlace(arr, a, b) {
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] <= a
+//       ? arr.splice(i, 1)
+//       : arr[i] >= b
+//       ? arr.splice(i, 1)
+//       : clg("No Change");
+//   }
+// }
+
+// let testArray = [5, 3, 8, 1];
+// filterRangeInPlace(testArray, 1, 4);
+// console.log(testArray);
+// // The function should only modify the array. It should not return anything.
+
+// // For instance:
+
+// // let arr = [5, 3, 8, 1];
+
+// // filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+// // alert( arr ); // [3, 1]
+// // Open a sandbox with tests.
+
+// // solution
+
+// // Sort in decreasing order
+// // importance: 4
+// let sortArr = [5, 2, 1, -10, 8];
+
+// // // ... your code to sort it in decreasing order
+
+// sortArr.sort((a, b) => a - b);
+// console.log(sortArr);
+
+// // alert( arr ); // 8, 5, 2, 1, -10
+// // solution
+// // Copy and sort array
+// // importance: 5
+
+// // We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
+
+// // Create a function copySorted(arr) that returns such a copy.
+
+// let programsArr = ["HTML", "JavaScript", "CSS"];
+
+// function copySorted(arr) {
+//   let outputArr = arr.slice(0);
+//   console.log(outputArr);
+//   return outputArr.sort((a, b) => a - b);
+// }
+
+// console.log(copySorted(programsArr));
+// console.log(programsArr);
+
+// let sorted = copySorted(arr);
+
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (no changes)
+// solution
+
+// Create an extendable calculator
+// importance: 5
+// Create a constructor function Calculator that creates “extendable” calculator objects.
+
+// The task consists of two parts.
+
+// First, implement the method calculate(str) that takes a string like "1 + 2" in the format “NUMBER operator NUMBER”
+// (space-delimited) and returns the result. Should understand plus + and minus -.
+
+// Usage example:
+
+// let calc = new Calculator;
+
+// alert( calc.calculate("3 + 7") ); // 10
+// Then add the method addMethod(name, func) that teaches the calculator a new operation.
+// It takes the operator name and the two-argument function func(a,b) that implements it.
+
+// For instance, let’s add the multiplication *, division / and power **:
+
+// let powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+
+// let result = powerCalc.calculate("2 ** 3");
+// alert( result ); // 8
+// No parentheses or complex expressions in this task.
+// The numbers and the operator are delimited with exactly one space.
+// There may be error handling if you’d like to add it.
+// Open a sandbox with tests.
+
+// solution
+// Map to names
+// importance: 5
+// You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+
+// For instance:
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// let names = /* ... your code */
+
+// alert( names ); // John, Pete, Mary
+// solution
+// Map to objects
+// importance: 5
+// You have an array of user objects, each one has name, surname and id.
+
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+// For instance:
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = /* ... your code ... */
+
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
+// So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
+
+// solution
+// Sort users by age
+// importance: 5
+// Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+// For instance:
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+// solution
+// Shuffle an array
+// importance: 3
+// Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+
+// Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+// let arr = [1, 2, 3];
+
+// shuffle(arr);
+// // arr = [3, 2, 1]
+
+// shuffle(arr);
+// // arr = [2, 1, 3]
+
+// shuffle(arr);
+// // arr = [3, 1, 2]
+// // ...
+// All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] or [3,1,2] etc, with equal probability of each case.
+
+// solution
+// Get average age
+// importance: 4
+// Write the function getAverageAge(users) that gets an array of objects with property age and returns the average age.
+
+// The formula for the average is (age1 + age2 + ... + ageN) / N.
+
+// For instance:
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+// solution
+// Filter unique array members
+// importance: 4
+// Let arr be an array.
+
+// Create a function unique(arr) that should return an array with unique items of arr.
+
+// For instance:
+
+// function unique(arr) {
+//   /* your code */
+// }
+
+// let strings = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+
+// alert( unique(strings) ); // Hare, Krishna, :-O
+// Open a sandbox with tests.
+
+// solution
+// Create keyed object from array
+// importance: 4
+// Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+// Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
+// For example:
+
+// let users = [
+//   {id: 'john', name: "John Smith", age: 20},
+//   {id: 'ann', name: "Ann Smith", age: 24},
+//   {id: 'pete', name: "Pete Peterson", age: 31},
+// ];
+
+// let usersById = groupById(users);
+
+// /*
+// // after the call we should have:
+
+// usersById = {
+//   john: {id: 'john', name: "John Smith", age: 20},
+//   ann: {id: 'ann', name: "Ann Smith", age: 24},
+//   pete: {id: 'pete', name: "Pete Peterson", age: 31},
+// }
+// */
+// Such function is really handy when working with server data.
+
+// In this task we assume that id is unique. There may be no two array items with the same id.
+
+// Please use array .reduce method in the solution.
+
+// ****************************************************************************************
+//
+//   The JS Way - Chapter 7
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding time!
+// Create all these programs in a generic fashion: the program output should reflect any update in the array's content.
+
+// Musketeers
+// Write a program that:
+
+// Creates an array named musketeers containing values "Athos", "Porthos" and "Aramis".
+// Shows each array element using a for loop.
+// Adds the "D'Artagnan" value to the array.
+// Shows each array element using the forEach() method.
+// Remove poor Aramis.
+// Shows each array element using a for-of loop.
+// Sum of values
+// Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
+
+// const values = [3, 11, 7, 2, 9, 10];
+// Array maximum
+// Write a program that creates the following array, then calculates and shows the array's maximum value.
+
+// const values = [3, 11, 7, 2, 9, 10];
+// List of words
+// Write a program that asks the user for a word until the user types "stop". The program then shows each of these words, except "stop".
