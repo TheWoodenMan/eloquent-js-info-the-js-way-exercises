50585acba81edd0f9bc983cc77b9600a8a5ce531
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-07-19 23:24:01 +0100

    js.info - objects the basics
    reading and tasks

--- js/main.js
+++ js/main.js
@@ -1080,3 +1080,131 @@
 //   }
 // }
 // wordList();
+
+//******************************************************/
+//*
+//*  https://javascript.info/object
+//*
+//******************************************************/
+
+// for (
+//   let i = 0, outputSource = "", outputRatings = "";
+//   i < Data.Ratings.length;
+//   i++
+// ) {
+//   outputSource += dataRatings[i].Source + " ";
+//   console.log(outputSource); // <--- this is for debugging
+//   document.querySelector("#rated").innerText = outputSource;
+
+//   outputRatings += " " + dataRatings[i].Value;
+//   console.log(outputRatings); // <--- this is for debugging
+//   document.querySelector("#ratedVal").innerText = outputRatings;
+// }
+
+// Tasks
+// Hello, object
+// importance: 5
+// Write the code, one line for each action:
+
+// Create an empty object user.
+let user = {};
+// Add the property name with the value John.
+user.name = "John";
+// Add the property surname with the value Smith.
+user.surname = "Smith";
+// Change the value of the name to Pete.
+user.name = "Pete";
+console.log(user.name);
+// Remove the property name from the object.
+delete user.name;
+// solution
+// Check for emptiness
+// importance: 5
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+let testObj = {};
+
+function isEmpty(obj) {
+  let outcome = true;
+  for (let key in obj) {
+    return false;
+  }
+  return outcome;
+}
+
+console.log(isEmpty(user));
+console.log(isEmpty(testObj));
+
+// NB the loop will not even start if there are no properties.
+
+//*********************************************** */
+
+// Should work like that:
+
+// let schedule = {};
+
+// alert( isEmpty(schedule) ); // true
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) ); // false
+// Open a sandbox with tests.
+
+// solution
+// Sum object properties
+// importance: 5
+// We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+function salarySum() {
+  let salaryTotal = 0;
+  for (let salary in salaries) {
+    salaryTotal += salaries[salary];
+  }
+
+  return salaryTotal;
+}
+
+console.log(salarySum(salaries));
+
+// If salaries is empty, then the result must be 0.
+
+// solution
+// Multiply numeric property values by 2
+// importance: 3
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+function multiplyNumeric(obj) {
+  for (let num in obj) {
+    if (typeof obj[num] === "number") {
+      obj[num] *= 2;
+    }
+  }
+}
+
+// For instance:
+
+// // before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+multiplyNumeric(menu);
+console.log(menu);
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+// P.S. Use typeof to check for a number here.
