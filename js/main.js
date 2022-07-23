6a611576fc01840ae138a95fa47d3bc58714ff8a
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-07-23 18:42:52 +0100

    completed js30
    js 30 array cardio done

--- js/main.js
+++ js/main.js
@@ -1107,104 +1107,104 @@
 // Write the code, one line for each action:
 
 // Create an empty object user.
-let user = {};
-// Add the property name with the value John.
-user.name = "John";
-// Add the property surname with the value Smith.
-user.surname = "Smith";
-// Change the value of the name to Pete.
-user.name = "Pete";
-console.log(user.name);
-// Remove the property name from the object.
-delete user.name;
-// solution
-// Check for emptiness
-// importance: 5
-// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+// let user = {};
+// // Add the property name with the value John.
+// user.name = "John";
+// // Add the property surname with the value Smith.
+// user.surname = "Smith";
+// // Change the value of the name to Pete.
+// user.name = "Pete";
+// console.log(user.name);
+// // Remove the property name from the object.
+// delete user.name;
+// // solution
+// // Check for emptiness
+// // importance: 5
+// // Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
 
-let testObj = {};
+// let testObj = {};
 
-function isEmpty(obj) {
-  let outcome = true;
-  for (let key in obj) {
-    return false;
-  }
-  return outcome;
-}
+// function isEmpty(obj) {
+//   let outcome = true;
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return outcome;
+// }
 
-console.log(isEmpty(user));
-console.log(isEmpty(testObj));
+// console.log(isEmpty(user));
+// console.log(isEmpty(testObj));
 
-// NB the loop will not even start if there are no properties.
+// // NB the loop will not even start if there are no properties.
 
-//*********************************************** */
+// //*********************************************** */
 
-// Should work like that:
+// // Should work like that:
 
-// let schedule = {};
+// // let schedule = {};
 
-// alert( isEmpty(schedule) ); // true
+// // alert( isEmpty(schedule) ); // true
 
-// schedule["8:30"] = "get up";
+// // schedule["8:30"] = "get up";
 
-// alert( isEmpty(schedule) ); // false
-// Open a sandbox with tests.
+// // alert( isEmpty(schedule) ); // false
+// // Open a sandbox with tests.
 
-// solution
-// Sum object properties
-// importance: 5
-// We have an object storing salaries of our team:
+// // solution
+// // Sum object properties
+// // importance: 5
+// // We have an object storing salaries of our team:
 
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130,
-};
-// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
-
-function salarySum() {
-  let salaryTotal = 0;
-  for (let salary in salaries) {
-    salaryTotal += salaries[salary];
-  }
-
-  return salaryTotal;
-}
-
-console.log(salarySum(salaries));
-
-// If salaries is empty, then the result must be 0.
-
-// solution
-// Multiply numeric property values by 2
-// importance: 3
-// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
-
-function multiplyNumeric(obj) {
-  for (let num in obj) {
-    if (typeof obj[num] === "number") {
-      obj[num] *= 2;
-    }
-  }
-}
-
-// For instance:
-
-// // before the call
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu",
-};
-
-multiplyNumeric(menu);
-console.log(menu);
-// // after the call
-// menu = {
-//   width: 400,
-//   height: 600,
-//   title: "My menu"
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
 // };
-// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+// // Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
 
-// P.S. Use typeof to check for a number here.
+// function salarySum() {
+//   let salaryTotal = 0;
+//   for (let salary in salaries) {
+//     salaryTotal += salaries[salary];
+//   }
+
+//   return salaryTotal;
+// }
+
+// console.log(salarySum(salaries));
+
+// // If salaries is empty, then the result must be 0.
+
+// // solution
+// // Multiply numeric property values by 2
+// // importance: 3
+// // Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+// function multiplyNumeric(obj) {
+//   for (let num in obj) {
+//     if (typeof obj[num] === "number") {
+//       obj[num] *= 2;
+//     }
+//   }
+// }
+
+// // For instance:
+
+// // // before the call
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+// };
+
+// multiplyNumeric(menu);
+// console.log(menu);
+// // // after the call
+// // menu = {
+// //   width: 400,
+// //   height: 600,
+// //   title: "My menu"
+// // };
+// // Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+// // P.S. Use typeof to check for a number here.
